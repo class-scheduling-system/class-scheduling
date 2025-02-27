28f41ec4480f84caceb2bc78588bac6b7678ac4c
qiyu <2845872182@qq.com>
2025-02-27 15:57:22 +0800

    feat(scheduling): 添加角色管理功能
    - 新增 RoleController 类，提供角色信息获取的 RESTful API
    - 新增 RoleService 接口和 RoleLogic 实现类，用于处理角色相关业务逻辑
    - 在 RoleMapper 中添加角色信息查询的 SQL 语句
    - 实现了根据角色 ID 或名称获取角色详细信息的功能

---
+++ src/main/java/com/frontleaves/scheduling/controllers/RoleController.java
@@ -0,0 +1,53 @@
+package com.frontleaves.scheduling.controllers;
+
+import com.frontleaves.scheduling.models.dto.RoleDTO;
+import com.frontleaves.scheduling.services.RoleService;
+import com.xlf.utility.BaseResponse;
+import com.xlf.utility.ErrorCode;
+import com.xlf.utility.ResultUtil;
+import com.xlf.utility.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 角色控制器
+ * <p>
+ * 该类提供了处理角色相关请求的 RESTful API，包括获取角色信息等功能。
+ *
+ * @author qiyu
+ * @version v1.0.0
+ * @since v1.0.0
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/role")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    /**
+     * 获取角色信息
+     * <p>
+     * 该方法用于获取指定角色的详细信息。通过调用 {@code roleService.getRole()} 方法从系统数据库中获取指定角色的详细信息，并将其封装到一个 {@code RoleDTO} 对象中返回。
+     * 返回的信息包括角色ID、角色名称、角色描述、角色权限等。
+     *
+     * @param role 角色ID或角色名称
+     * @return 包含角色详细信息的响应实体，其中数据部分为 {@code BaseResponse<RoleDTO>} 类型
+     */
+    @GetMapping("")
+    public ResponseEntity<BaseResponse<RoleDTO>> getRole(
+            @RequestParam("role_uuid") String role
+    ) {
+        if (role == null || role.isBlank()) {
+            throw new BusinessException("角色ID/名称不能为空", ErrorCode.PARAMETER_ERROR);
+        }
+        RoleDTO roleDTO = roleService.getRole(role);
+        return ResultUtil.success("角色信息获取成功", roleDTO);
+    }
+}
