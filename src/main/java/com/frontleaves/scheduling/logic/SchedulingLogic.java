882285c64830bdb3f2dd49a3eca113926477fe3d
FLASHLACK1314 <zlp18021547586@163.com>
2025-03-24 16:25:33 +0800

    feat(scheduling): 添加调度服务和逻辑类
    - 新增 SchedulingService 接口
    - 实现 SchedulingLogic 类

---
+++ src/main/java/com/frontleaves/scheduling/logic/SchedulingLogic.java
@@ -0,0 +1,10 @@
+package com.frontleaves.scheduling.logic;
+
+import com.frontleaves.scheduling.services.SchedulingService;
+
+/**
+ * 调度逻辑
+ * @author FLASHLACK
+ */
+public class SchedulingLogic implements SchedulingService {
+}
---
+++ src/main/java/com/frontleaves/scheduling/services/SchedulingService.java
@@ -0,0 +1,8 @@
+package com.frontleaves.scheduling.services;
+
+/**
+ * 调度服务
+ * @author FLASHLACK
+ */
+public interface SchedulingService {
+}
