911650df0e6f3d76c7ee81ce285dd0c8b28076fb
FLASHLACK1314 <zlp18021547586@163.com>
2025-03-25 22:38:51 +0800

    feat(scheduling): 添加学期管理相关实体、DAO和Mapper
    - 新增 SemesterDO 实体类，用于表示学期信息
    - 新增 SemesterDAO 类，提供学期数据访问功能
    - 新增 SemesterMapper 接口，定义学期数据映射

---
+++ src/main/java/com/frontleaves/scheduling/daos/SemesterDAO.java
@@ -0,0 +1,18 @@
+package com.frontleaves.scheduling.daos;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.frontleaves.scheduling.mappers.SemesterMapper;
+import com.frontleaves.scheduling.models.entity.SemesterDO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 学期数据访问对象
+ * @author FLASHLACK
+ */
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class SemesterDAO  extends ServiceImpl<SemesterMapper, SemesterDO> {
+}
---
+++ src/main/java/com/frontleaves/scheduling/models/entity/SemesterDO.java
@@ -0,0 +1,70 @@
+package com.frontleaves.scheduling.models.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+/**
+ * 学期DO
+ *
+ * @author FLASHLACK
+ */
+@Data
+@TableName("cs_semester")
+public class SemesterDO {
+    /**
+     * 学期主键
+     */
+    @TableId(value = "semester_uuid", type = IdType.ASSIGN_UUID)
+    private String semesterUuid;
+
+    /**
+     * 学期名称
+     */
+    @TableField("name")
+    private String name;
+
+    /**
+     * 学期描述
+     */
+    @TableField("description")
+    private String description;
+
+    /**
+     * 学期开始日期
+     */
+    @TableField("start_date")
+    private Date startDate;
+
+    /**
+     * 学期结束日期
+     */
+    @TableField("end_date")
+    private Date endDate;
+
+    /**
+     * 是否当前学期
+     */
+    @TableField("is_current")
+    private Boolean isCurrent;
+
+    /**
+     * 是否启用
+     */
+    @TableField("is_enabled")
+    private Boolean isEnabled;
+
+    /**
+     * 创建时间
+     */
+    @TableField(value = "created_at", fill = FieldFill.INSERT)
+    private Timestamp createdAt;
+
+    /**
+     * 更新时间
+     */
+    @TableField(value = "updated_at", fill = FieldFill.INSERT_UPDATE)
+    private Timestamp updatedAt;
+}
