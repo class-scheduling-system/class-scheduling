358cdefd3d03a32ba04c87018a3bbbe0dc7525f1
FLASHLACK1314 <zlp18021547586@163.com>
2025-03-27 23:43:57 +0800

    test(scheduling): 添加课程类型单元测试
    - 新增 CourseTypeDAO 和 CourseTypeService 的测试用例
    - 验证通过 UUID 获取课程类型的功能
    - 测试 CourseTypeService 中带错误处理的获取课程类型方法

---
+++ src/test/java/com/frontleaves/scheduling/dao/CourseTypeTest.java
@@ -0,0 +1,26 @@
+package com.frontleaves.scheduling.dao;
+
+import com.frontleaves.scheduling.daos.CourseTypeDAO;
+import com.frontleaves.scheduling.models.entity.CourseTypeDO;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class CourseTypeTest {
+
+    @Resource
+    private CourseTypeDAO courseTypeDAO;
+
+
+    @Test
+    void testGetCourseTypeByUuid (){
+        log.debug("测试获取课程类型信息");
+        CourseTypeDO courseTypeDO = courseTypeDAO.lambdaQuery().list().get(0);
+        CourseTypeDO courseTypeDO1 = courseTypeDAO.getCourseTypeByUuid(courseTypeDO.getCourseTypeUuid());
+        Assertions.assertNotNull(courseTypeDO1);
+    }
+}
