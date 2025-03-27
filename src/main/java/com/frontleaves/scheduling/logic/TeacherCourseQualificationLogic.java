66eb1332386baf51e3d6aaa9d8e3fece368a990d
FLASHLACK1314 <zlp18021547586@163.com>
2025-03-27 17:04:43 +0800

    feat(scheduling): 添加教师课程资格服务
    - 新增 TeacherCourseQualificationService 接口
    - 实现 TeacherCourseQualificationLogic 类- 提供教师课程资格相关的业务逻辑处理

---
+++ src/main/java/com/frontleaves/scheduling/logic/TeacherCourseQualificationLogic.java
@@ -0,0 +1,22 @@
+package com.frontleaves.scheduling.logic;
+
+import com.frontleaves.scheduling.services.TeacherCourseQualificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 教师课程资格业务逻辑实现类
+ * <p>
+ * 该类实现了TeacherCourseQualificationService接口，提供教师课程资格相关的业务逻辑处理。
+ * </p>
+ *
+ * @author FLASHLACK
+ * @version v1.0.0
+ * @since v1.0.0
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TeacherCourseQualificationLogic implements TeacherCourseQualificationService {
+}
---
+++ src/main/java/com/frontleaves/scheduling/services/TeacherCourseQualificationService.java
@@ -0,0 +1,17 @@
+package com.frontleaves.scheduling.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 教师课程资格服务接口
+ * <p>
+ * 该接口定义了处理教师课程资格相关业务的方法。
+ * </p>
+ *
+ * @author FLASHLACK
+ * @version v1.0.0
+ * @since v1.0.0
+ */
+@Service
+public interface TeacherCourseQualificationService {
+}
